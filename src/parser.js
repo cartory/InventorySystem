e8fffdce23f7206d074ed179d773bd8c36e7fe6a
cartory <xXcartoryXx@gmail.com>
2021-07-31 03:57:27 -0400

    update parser.js

--- src/parser.js
+++ src/parser.js
@@ -5,16 +5,16 @@ let database = Project.Models.Model.ModelChildren.DBTable.map(table => {
 	let attribs = table.ModelChildren.DBColumn.map(column => {
 		let { ForeignKeyConstraints } = column
 		return {
-			Id: column.Id,
-			Name: column.Name,
-			Type: column.Type,
-			Length: column.Length,
-			Unique: column.Unique,
-			Nullable: column.Nullable,
-			PrimaryKey: column.PrimaryKey,
-			DefaultValue: column.DefaultValue,
-			IdentityIncrement: column.IdentityIncrement,
-			ForeignKeyConstraints: ForeignKeyConstraints?.DBForeignKeyConstraint['RefColumn'],
+			id: column.Id,
+			name: column.Name,
+			type: column.Type,
+			length: column.Length,
+			unique: column.Unique,
+			nullable: column.Nullable,
+			primaryKey: column.PrimaryKey,
+			defaultValue: column.DefaultValue,
+			identityIncrement: column.IdentityIncrement,
+			foreignKey: ForeignKeyConstraints?.DBForeignKeyConstraint['RefColumn'],
 		};
 	})
 	return {

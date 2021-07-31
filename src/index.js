ad8c3df37875699c5e8e8875d9142ed7f27cd0b7
cartory <xXcartoryXx@gmail.com>
2021-07-31 03:58:19 -0400

    mapping json with DFS

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// const fs = require('fs')
+const database = require('../assets/info.json')
+
+let DB = database.map(table => {
+	table.marked = false
+	return table
+})
+
+const getForeignKeys = ({ attribs }) => {
+	return attribs
+		.filter(column => column.foreignKey)
+		.map(({ foreignKey }) => foreignKey)
+}
+
+const getReferencedTables = (tables, foreignKeys) => {
+	return tables.filter(({ attribs }) => {
+		return attribs.filter(({ id }) => foreignKeys.includes(id)).length
+	})
+}
+
+const generateFiles = (tables) => {
+	tables.forEach(table => {
+		if (!table.marked) {
+			let foreignKeys = getForeignKeys(table)
+			let referencedTables = getReferencedTables(DB, foreignKeys)
+			generateFiles(referencedTables)
+			table.marked = true
+			console.log(table.tableName);
+		}
+	})
+}
+
+generateFiles(DB)

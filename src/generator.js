3d7b4a4ac8afba7e6575319392d938c9589e74cd
cartory <xXcartoryXx@gmail.com>
2021-07-31 21:18:25 -0400

    generator.js > generateModel

--- src/generator.js
+++ src/generator.js
@@ -1,21 +1,53 @@
 const fs = require('fs')
+const modelJSON = require('./json/model.json')
+
+const columnsToDrop = [
+	'id', 'name', 'length', 'foreignKey'
+]
+
+const getDataType = {
+	char: (_) => `DataTypes.CHAR`,
+	byte: (_) => `DataTypes.TINYINT`,
+	boolean: (_) => `DataTypes.BOOLEAN`,
+
+	float: (length) => `DataTypes.FLOAT(${length})`,
+	double: (length) => `DataTypes.DOUBLE(${length})`,
+
+	int: (length) => `DataTypes.INTEGER(${length})`,
+	long: (length) => `DataTypes.BIGINT(${length})`,
+	short: (length) => `DataTypes.SMALLINT( ${length})`,
+
+	text: (length) => `DataTypes.TEXT(${length})`,
+	string: (length) => `DataTypes.STRING(${length})`,
+	varchar: (length) => `DataTypes.STRING(${length})`,
+
+	date: (_) => `DataTypes.DATE`,
+	timestamp: (_) => `DataTypes.TIME`,
+}
 
 const generateModel = (table) => {
 	let className = table.tableName
-	let model = (`
-const { DataTypes, Model } = require('sequelize')\nconst sequelize = require('../orm/sequelize.config')
-class ${className} extends Model { }
-		
-${className}.init({
-	// MUST DECLARE ATTRIBS
-}, {
-	sequelize
-})
+	let modelFile = `const { Model, DataTypes } = require('sequelize')\n`
+	modelFile += `const sequelize = require('../orm/sequelize.config.js')\n\n`
 
-module.exports = ${className}
-	`)
+	modelFile += `class ${className} extends Model { }\n\n`
 
-	fs.writeFileSync(`src/models/${className}.js`, model, {
+	modelFile += `${className}.init({\n ${table.attribs.map((column) => {
+		let { name, length } = column
+
+		columnsToDrop.forEach(col => delete column[col])
+
+		return `\t${name}: {\n${Object.keys(column).map(k => {
+			if (k === 'type') {
+				return `\t\t${k}: ${getDataType[column[k]](length)}`
+			}
+			return `\t\t${k}: ${column[k]}`
+		}).join(',\n')}\n\t}`
+	}).join(',\n')}\n}, { sequelize })\n\n`
+
+	modelFile += `${modelJSON.exports} = ${className}\n`
+
+	fs.writeFileSync(`src/models/${className}.js`, modelFile, {
 		encoding: 'utf-8'
 	})
 }

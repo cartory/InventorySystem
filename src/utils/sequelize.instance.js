21eacb2f2e7116d7a7b3839f20d5ee71f7d8cbec
cartory <xXcartoryXx@gmail.com>
2021-08-01 01:26:11 -0400

    add sequelizeInstance

---
+++ src/utils/sequelize.instance.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize')
+
+require('dotenv').config()
+
+module.exports = new Sequelize({
+	logging: false,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	dialect: process.env.DB_DIAL,
+	database: process.env.DB_NAME,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	define: {
+		paranoid: true,
+		defaultScope: {
+			attributes: {
+				exclude: [
+					'createdAt', 'updatedAt', 'deletedAt'
+				],
+			},
+		},
+	},
+})

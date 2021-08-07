431c1cfa6e2ec58577c2239ea8f53236d4b1e81e
cartory <xXcartoryXx@gmail.com>
2021-08-07 01:02:36 -0400

    delete & ignore

--- src/controllers/Customer_MovieController.js
+++
@@ -1,7 +0,0 @@
-const { Controller } = require('../utils/controller')
-const Customer_Movie = require('../models/Customer_Movie')
-
-class Customer_MovieController extends Controller {
-	constructor() {		super(Customer_Movie)	}}
-
-module.exports = new Customer_MovieController()
--- src/controllers/ProducerController.js
+++
@@ -1,7 +0,0 @@
-const { Controller } = require('../utils/controller')
-const Producer = require('../models/Producer')
-
-class ProducerController extends Controller {
-	constructor() {		super(Producer)	}}
-
-module.exports = new ProducerController()

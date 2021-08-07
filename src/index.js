afb46e21c2ac275f30ccca2b7ccdccc371cdb214
cartory <xXcartoryXx@gmail.com>
2021-08-07 04:09:09 -0400

    update src/index

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const app = require('./app')
 require('dotenv').config()
 
 const run = () => {
-	if (process.env.NODE_ENV === 'development') {
-		app.use(require('morgan')('dev'))
-	}
+	// if (process.env.NODE_ENV === 'development') {
+	// 	app.use(require('morgan')('dev'))
+	// }
 
 	app.listen(process.env.PORT || 8000, () => {
 		console.log(`Server running on \x1b[33mhttp://${process.env.HOST}:${process.env.PORT}\x1b[0m`);

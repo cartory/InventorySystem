9ff6ce8c21a1f2a0eb0869f9bd70222a09ac03c3
cartory <48525396+cartory@users.noreply.github.com>
2021-08-07 00:56:37 -0400

    Delete src/controllers directory

--- src/controllers/CustomerController.js
+++
@@ -1,7 +0,0 @@
-const { Controller } = require('../utils/controller')
-const Customer = require('../models/Customer')
-
-class CustomerController extends Controller {
-	constructor() {		super(Customer)	}}
-
-module.exports = new CustomerController()
--- src/controllers/MovieController.js
+++
@@ -1,7 +0,0 @@
-const { Controller } = require('../utils/controller')
-const Movie = require('../models/Movie')
-
-class MovieController extends Controller {
-	constructor() {		super(Movie)	}}
-
-module.exports = new MovieController()

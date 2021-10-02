e7ad08f86a6a7c521471aeddd4ad9d16d5c63780
cartory <xXcartoryXx@gmail.com>
2021-10-02 14:52:47 -0400

    update sequelize > env.DATABASE_URL

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -2,22 +2,18 @@ const { Sequelize } = require('sequelize')
 
 require('dotenv').config()
 
-module.exports = new Sequelize({
-	logging: true,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	dialect: process.env.DB_DIAL,
-	database: process.env.DB_NAME,
-	username: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	define: {
-		paranoid: true,
-		defaultScope: {
-			attributes: {
-				exclude: [
-					'createdAt', 'updatedAt', 'deletedAt'
-				],
-			},
-		},
-	},
-})
+module.exports = new Sequelize(
+	process.env.DATABASE_URL,
+	{
+		define: {
+			paranoid: true,
+			defaultScope: {
+				attributes: {
+					exclude: [
+						'createdAt', 'updatedAt', 'deletedAt'
+					]
+				}
+			}
+		}
+	}
+)

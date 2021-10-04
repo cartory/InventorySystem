e9d15360978d413c1f2c43b100425f6648c46fac
cartory <xXcartoryXx@gmail.com>
2021-10-04 08:19:15 -0400

    delete teacher

--- src/controllers/TeacherController.js
+++
@@ -1,7 +0,0 @@
-const { Controller } = require('../utils/controller')
-const Teacher = require('../models/Teacher')
-
-class TeacherController extends Controller {
-	constructor() {		super(Teacher)	}}
-
-module.exports = new TeacherController()

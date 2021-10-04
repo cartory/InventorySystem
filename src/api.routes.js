dffb56dc32fa523329c46151b517788fd3588f9e
cartory <xXcartoryXx@gmail.com>
2021-10-04 08:15:53 -0400

    update api.routes

--- src/api.routes.js
+++ src/api.routes.js
@@ -2,25 +2,17 @@ const { Router } = require('express')
 const { addOperationRoutes } = require('./utils/routes.utils')
 
 const controllers = [
-	require('./controllers/PlaceController'),
 	require('./controllers/UserController'),
 	require('./controllers/TaskController'),
-	require('./controllers/User_PlaceController'),
-	require('./controllers/User_TaskController'),
-	require('./controllers/TeacherController'),
 	require('./controllers/TypeController'),
-	require('./controllers/Place_PlaceController')
+	require('./controllers/PlaceController')
 ]
 
 const routes = [
-	require('./routes/Place.route.json'),
 	require('./routes/User.route.json'),
 	require('./routes/Task.route.json'),
-	require('./routes/User_Place.route.json'),
-	require('./routes/User_Task.route.json'),
-	require('./routes/Teacher.route.json'),
 	require('./routes/Type.route.json'),
-	require('./routes/Place_Place.route.json')
+	require('./routes/Place.route.json')
 ]
 
 const router = Router()

3e21c27b9abcaecd0541a34978edd0baba37f853
cartory <xXcartoryXx@gmail.com>
2021-10-04 18:05:13 -0400

    update typeController > with places

--- src/controllers/TypeController.js
+++ src/controllers/TypeController.js
@@ -2,6 +2,24 @@ const { Controller } = require('../utils/controller')
 const { Type } = require('../utils/models')
 
 class TypeController extends Controller {
-	constructor() {		super(Type)	}}
+	constructor() {
+		super(Type)
+	}
+
+	find = async ({ params }, res) => {
+		return Type
+			.findOne({
+				where: { id: params.id },
+				include: [
+					'places'
+				]
+			})
+			.then(type => res.status(200).json(type))
+			.catch(err => {
+				console.error(err);
+				return res.status(500).json(this.defaultErrorMessage)
+			})
+	}
+}
 
 module.exports = new TypeController()

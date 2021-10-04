b146a9e19c922240596103e54218f69456b351a3
cartory <xXcartoryXx@gmail.com>
2021-10-04 08:19:03 -0400

    delete logical > controllers

--- src/controllers/User_TaskController.js
+++
@@ -1,7 +0,0 @@
-const { Controller } = require('../utils/controller')
-const User_Task = require('../models/User_Task')
-
-class User_TaskController extends Controller {
-	constructor() {		super(User_Task)	}}
-
-module.exports = new User_TaskController()

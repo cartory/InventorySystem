3c4edbb55657f368ce581a1e3532a797f1e6113e
cartory <xXcartoryXx@gmail.com>
2021-10-04 10:13:18 -0400

    update placeController > with assocs

--- src/controllers/PlaceController.js
+++ src/controllers/PlaceController.js
@@ -2,6 +2,27 @@ const { Controller } = require('../utils/controller')
 const { Place } = require('../utils/models')
 
 class PlaceController extends Controller {
-	constructor() {		super(Place)	}}
+	constructor() {
+		super(Place)
+	}
+
+	find = async ({ params }, res) => {
+		return Place
+			.findOne({
+				where: { id: params.id },
+				include: [
+					'type',
+					'users',
+					'tasks',
+					'subPlaces',
+				],
+			})
+			.then(place => res.status(200).json(place))
+			.catch(err => {
+				console.log(err);
+				return res.status(500).json(this.defaultErrorMessage)
+			})
+	}
+}
 
 module.exports = new PlaceController()

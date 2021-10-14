0473c5a32db56f5adbe9a6dd36942d1f52e7afc8
cartory <xXcartoryXx@gmail.com>
2021-10-14 14:50:21 -0400

    update index > default PORT

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const run = () => {
 	// 	app.use(require('morgan')('dev'))
 	// }
 
-	app.listen(process.env.PORT || 8000, () => {
+	app.listen(process.env.PORT || 3000, () => {
 		console.log(`Server running on \x1b[33mhttp://${process.env.HOST}:${process.env.PORT}\x1b[0m`);
 		console.log(new Date());
 	})

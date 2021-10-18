f197cca02351577b42d33d9b0e7feb8a41442f03
cartory <xXcartoryXx@gmail.com>
2021-10-17 23:18:05 -0400

    update EquipmentController > all

--- src/controllers/EquipmentController.js
+++ src/controllers/EquipmentController.js
@@ -1,7 +1,36 @@
 const { Controller } = require('../utils/controller')
-const { Equipment } = require('../utils/models')
+const { Equipment, Movement } = require('../utils/models')
 
 class EquipmentController extends Controller {
-	constructor() {		super(Equipment)	}}
+	constructor() {
+		super(Equipment)
+	}
+
+	all = ({ query }, res) => {
+		let { page = 0, limit = 10 } = query
+		limit = Number.parseInt(limit) ?? 10
+
+		return Equipment
+			.findAll({
+				limit,
+				offset: page * limit,
+				include: [
+					'unit',
+					{
+						model: Movement,
+						as: 'movements',
+						include: [
+							'reason', 'placeFrom', 'placeTo',
+						],
+					},
+				],
+			})
+			.then(equipment => res.status(200).json(equipment))
+			.catch(err => {
+				console.error(err);
+				return res.status(500).json(this.defaultErrorMessage)
+			})
+	}
+}
 
 module.exports = new EquipmentController()

565179a7dc8031b16a79ff513fb73bb3e98eaa54
cartory <xXcartoryXx@gmail.com>
2021-10-17 22:33:50 -0400

    update main_seeder > seedEquipments

--- seeders/main_seeder.js
+++ seeders/main_seeder.js
@@ -1,11 +1,12 @@
 const sequelize = require('../src/utils/sequelize')
+
 // const faker = require('faker')
 
 const data = require('./data.json')
 const { modules } = require('./csv_parser')
 
 const {
-	Unit, Reason, Equipment,
+	Unit, Reason, Equipment, Movement,
 	Place, Type, Place_Place,
 } = require('../src/utils/models')
 
@@ -38,16 +39,25 @@ const seedPlaces = ({ places = [], placeParent_id = null }) => {
 	})
 }
 
-const seedEquipments = ({ module, rooms = [] }) => {
+const seedEquipments = ({ module, rooms = [], Reasonid }) => {
 	rooms.forEach(room => {
 		const equipments = require(`./json/${module}-${room}.json`)
 		equipments.forEach(async equipment => {
 			try {
-				let unit = await Unit.findOne({ where: { name: equipment.Unitid }, logging: false })
-				await Equipment.create({
+				let placeTo = await Place.findOne({ where: { code: room } })
+				let unit = await Unit.findOne({ where: { name: equipment.Unitid } })
+
+				equipment = await Equipment.create({
 					...equipment,
 					Unitid: unit?.getDataValue('id'),
 				})
+
+				await Movement.create({
+					Reasonid,
+					placeTo_id: placeTo?.getDataValue('id'),
+					Equipmentid: equipment?.getDataValue('id'),
+				})
+
 			} catch (error) {
 				console.error(error)
 				process.exit(0)
@@ -67,7 +77,14 @@ sequelize
 			await Type.bulkCreate(data.place_type.map(name => ({ name })))
 
 			seedPlaces({ places: data.place })
-			modules.forEach(([module, rooms]) => seedEquipments({ module, rooms }))
+
+			let reason = await Reason.findOne({ where: { name: 'transaction' } })
+			modules.forEach(([module, rooms]) => {
+				seedEquipments({
+					module, rooms,
+					Reasonid: reason?.getDataValue('id')
+				})
+			})
 		} catch (err) {
 			console.error(err);
 			process.exit(0)

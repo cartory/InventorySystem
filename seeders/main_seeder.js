daee385a45858dc559d6cd66fbdd132314aeeac0
cartory <xXcartoryXx@gmail.com>
2021-10-17 21:19:52 -0400

    update main_seeder > seedPlaces

--- seeders/main_seeder.js
+++ seeders/main_seeder.js
@@ -9,7 +9,7 @@ const {
 	Place, Type, Place_Place,
 } = require('../src/utils/models')
 
-const seedPlaces = ({ places = [], placeParent_id = null, transaction }) => {
+const seedPlaces = ({ places = [], placeParent_id = null }) => {
 	places.forEach(async ([code, name, type, children]) => {
 		try {
 			type = await Type.findOne({ where: { name: type } })
@@ -29,7 +29,6 @@ const seedPlaces = ({ places = [], placeParent_id = null, transaction }) => {
 			}
 
 			seedPlaces({
-				transaction,
 				places: children,
 				placeParent_id: place.getDataValue('id'),
 			})
@@ -44,15 +43,11 @@ const seedEquipments = ({ module, rooms = [] }) => {
 		const equipments = require(`./json/${module}-${room}.json`)
 		equipments.forEach(async equipment => {
 			try {
-				let unit = await Unit.findOne({ where: { name: equipment.UND }, logging: false })
-				await Equipment
-					.create({
-						code: equipment.CODIGO,
-						state: equipment.ESTADO,
-						description: equipment.DESCRIPCION,
-						observations: equipment.OBSERVACION,
-						Unitid: unit?.getDataValue('id'),
-					})
+				let unit = await Unit.findOne({ where: { name: equipment.Unitid }, logging: false })
+				await Equipment.create({
+					...equipment,
+					Unitid: unit?.getDataValue('id'),
+				})
 			} catch (error) {
 				console.error(error)
 				process.exit(0)
@@ -66,16 +61,14 @@ sequelize
 	.then(async () => {
 		await sequelize.dropAllSchemas({ logging: true })
 		await sequelize.sync({ alter: true })
-		let transaction = await sequelize.transaction({ autocommit: true, logging: true })
 		try {
 			await Unit.bulkCreate(data.unit.map(name => ({ name })))
 			await Reason.bulkCreate(data.reason.map(name => ({ name })))
 			await Type.bulkCreate(data.place_type.map(name => ({ name })))
 
-			seedPlaces({ places: data.place, transaction })
+			seedPlaces({ places: data.place })
 			modules.forEach(([module, rooms]) => seedEquipments({ module, rooms }))
 		} catch (err) {
-			await transaction.rollback()
 			console.error(err);
 			process.exit(0)
 		}

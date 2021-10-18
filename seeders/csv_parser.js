26142c50f2af600285bce9d0a89940895cf9c2fc
cartory <xXcartoryXx@gmail.com>
2021-10-17 21:19:06 -0400

    update csv_parser

--- seeders/csv_parser.js
+++ seeders/csv_parser.js
@@ -1,9 +1,15 @@
 const fs = require('fs')
 const csv = require('csv-parser')
 
+const modules = [
+	[123, ['03']],
+	[232, ['09', 10, 11, 12]],
+	[236, [41, 42, 43, 44, 45, 46, 48]],
+]
+
 const csv2json = (module, rooms = []) => {
+	let results = []
 	if (rooms.length) {
-		let results = []
 		let room = rooms.pop()
 		let stream = fs.createReadStream(`${__dirname}/csv/${module}-${room}.csv`)
 
@@ -12,7 +18,13 @@ const csv2json = (module, rooms = []) => {
 			.on('error', err => console.error(err))
 			.on('data', data => {
 				delete data['']
-				results.push(data)
+				results.push({
+					code: data.CODIGO,
+					state: data.ESTADO,
+					description: data.DESCRIPCION,
+					observations: data.OBSERVACION,
+					Unitid: data.UND,
+				})
 			})
 			.on('end', () => {
 				stream.close()
@@ -22,20 +34,11 @@ const csv2json = (module, rooms = []) => {
 				})
 			})
 	}
-
 }
-
-const modules = [
-	[123, ['03']],
-	[232, ['09', 10, 11, 12]],
-	[236, [41, 42, 43, 44, 45, 46, 48]],
-]
 
 // modules.forEach(([module, rooms]) => {
 // 	csv2json(module, rooms)
 // 	console.log(module, 'csv2json Done!');
 // })
 
-module.exports = {
-	modules
-}
+module.exports = { modules }

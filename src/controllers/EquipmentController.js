d14844283878838948d88bb64ad63a005a0aefde
cartory <xXcartoryXx@gmail.com>
2021-10-26 16:30:14 -0400

    Updates

--- src/controllers/EquipmentController.js
+++ src/controllers/EquipmentController.js
@@ -1,9 +1,63 @@
+const { Op } = require('sequelize')
 const { Controller } = require('../utils/controller')
 const { Equipment, Movement, Place } = require('../utils/models')
 
 class EquipmentController extends Controller {
 	constructor() {
 		super(Equipment)
+	}
+
+	search = async (req, res) => {
+		const { query, placeId } = req.query;
+
+		try {
+			let equipments = await Equipment.findAll({
+				where: {
+					[Op.or]: [
+						{ code: { [Op.substring]: query } },
+						{ description: { [Op.substring]: query } },
+						{ observations: { [Op.substring]: query } },
+					],
+				},
+				include: [
+					{
+						association: 'unit',
+						where: {
+							[Op.or]: {
+								name: {
+									[Op.substring]: query
+								}
+							}
+						}
+					},
+					{
+						include: ['type'],
+						association: 'places',
+						through: { attributes: [] },
+						where: placeId ? { id: placeId } : null,
+					},
+					{
+						association: 'movements',
+						include: [
+							'reason',
+							{
+								include: ['type'],
+								association: 'placeTo',
+							},
+							{
+								include: ['type'],
+								association: 'placeFrom',
+							},
+						],
+					},
+				]
+			})
+
+			return res.status(200).json(equipments)
+		} catch (error) {
+			console.error(error);
+			return res.status(500).json(this.defaultErrorMessage)
+		}
 	}
 
 	all = async ({ query }, res) => {
@@ -16,26 +70,22 @@ class EquipmentController extends Controller {
 				include: [
 					'unit',
 					{
-						model: Place,
-						as: 'places',
+						association: 'places',
 						include: ['type'],
 						where: placeId ? { id: placeId } : null,
 						through: { attributes: [] }
 					},
 					{
-						model: Movement,
-						as: 'movements',
+						association: 'movements',
 						include: [
 							'reason',
 							{
-								model: Place,
-								as: 'placeTo',
-								include: ['type']
+								include: ['type'],
+								association: 'placeTo',
 							},
 							{
-								model: Place,
-								as: 'placeFrom',
-								include: ['type']
+								include: ['type'],
+								association: 'placeFrom',
 							},
 						],
 					},
@@ -56,25 +106,21 @@ class EquipmentController extends Controller {
 				include: [
 					'unit',
 					{
-						model: Place,
-						as: 'places',
 						include: ['type'],
+						association: 'places',
 						through: { attributes: [] }
 					},
 					{
-						model: Movement,
-						as: 'movements',
+						association: 'movements',
 						include: [
 							'reason',
 							{
-								model: Place,
-								as: 'placeTo',
-								include: ['type']
+								include: ['type'],
+								association: 'placeTo',
 							},
 							{
-								model: Place,
-								as: 'placeFrom',
-								include: ['type']
+								include: ['type'],
+								association: 'placeFrom',
 							},
 						],
 					},
